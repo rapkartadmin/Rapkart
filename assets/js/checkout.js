9fa1b0845e77d7e84f7eeab4defeacbef8b9f147
ChandrashekarML <chandrashekarml54321@gmail.com>
2025-12-30 15:22:19 +0530

    custom render checkout page

---
+++ assets/js/checkout.js
@@ -0,0 +1,289 @@
+const productArray = [
+    {
+        productId: 1,
+        productTitle:"Custom Moon Lamp",
+        productCost: 800,
+        productImage:[
+            "https://m.media-amazon.com/images/I/71ZqQVLSjdL._AC_UF350,350_QL80_.jpg",
+            "https://zocivoci.com/cdn/shop/files/91qpvX6OkpL._SL1500.jpg?v=1764847441",
+            "https://rukminim2.flixcart.com/image/480/640/xif0q/table-lamp/b/d/1/3d-personalized-photo-moon-light-7373105930-whatsapp-your-photo-original-imagkegrgkqzthfu.jpeg?q=90"
+        ],
+        productDimensions:"14cm x 14cm x 20cm",
+        productWeight:"250g",
+        productOutputVoltage:"12v",
+        productInputVoltage:"220v",
+        productColor:"White",
+        productFileFields:[
+            {
+                fieldName:"userImage",
+                fieldTitle:"Custom Image",
+            }
+        ],
+        productTextFields:[],
+        productExpectedDeliveryDate:5,
+        productMaterial:"Plastic"
+    },
+    {
+        productId: 2,
+        productTitle:"Custom Bed Lamp",
+        productCost: 1200,
+        productImage:["https://m.media-amazon.com/images/I/71ZqQVLSjdL._AC_UF350,350_QL80_.jpg","https://zocivoci.com/cdn/shop/files/91qpvX6OkpL._SL1500.jpg?v=1764847441",
+            "https://rukminim2.flixcart.com/image/480/640/xif0q/table-lamp/b/d/1/3d-personalized-photo-moon-light-7373105930-whatsapp-your-photo-original-imagkegrgkqzthfu.jpeg?q=90"
+        ],
+        productDimensions:"25cm x 25cm x 25cm",
+        productWeight:"500g",
+        productOutputVoltage:"12v",
+        productInputVoltage:"220v",
+        productColor:"White",
+        productFileFields:[
+            {
+                fieldName:"userImage",
+                fieldTitle:"Custom Image",
+            }
+        ],
+        productTextFields:[],
+        productExpectedDeliveryDate:7,
+        productMaterial:"Plastic"
+    },
+    {
+        productId: 3,
+        productTitle:"Custom Cube",
+        productCost: 777,
+        productImage:["https://m.media-amazon.com/images/I/71ZqQVLSjdL._AC_UF350,350_QL80_.jpg","https://zocivoci.com/cdn/shop/files/91qpvX6OkpL._SL1500.jpg?v=1764847441",
+            "https://rukminim2.flixcart.com/image/480/640/xif0q/table-lamp/b/d/1/3d-personalized-photo-moon-light-7373105930-whatsapp-your-photo-original-imagkegrgkqzthfu.jpeg?q=90"
+        ],
+        productDimensions:"30cm x 10cm x 5cm",
+        productWeight:"300g",
+        productOutputVoltage:"N/A",
+        productInputVoltage:"N/A",
+        productColor:"Black",
+        productFileFields:[],
+        productTextFields:[
+            {
+                fieldName:"customName",
+                fieldTitle:"Custom Name",
+            }
+        ],
+        productExpectedDeliveryDate:4,
+        productMaterial:"Plastic"
+    },
+    {
+        productId: 4,
+        productTitle:"Custom 3D Heart",
+        productCost: 999,
+        productImage:[
+            "https://m.media-amazon.com/images/I/71ZqQVLSjdL._AC_UF350,350_QL80_.jpg",
+            "https://zocivoci.com/cdn/shop/files/91qpvX6OkpL._SL1500.jpg?v=1764847441",
+            "https://rukminim2.flixcart.com/image/480/640/xif0q/table-lamp/b/d/1/3d-personalized-photo-moon-light-7373105930-whatsapp-your-photo-original-imagkegrgkqzthfu.jpeg?q=90"
+        ],
+        productDimensions:"14cm x 14cm x 20cm",
+        productWeight:"250g",
+        productOutputVoltage:"12v",
+        productInputVoltage:"220v",
+        productColor:"White",
+        productFileFields:[
+            {
+                fieldName:"userImage",
+                fieldTitle:"Custom Image",
+            }
+        ],
+        productTextFields:[],
+        productExpectedDeliveryDate:5,
+        productMaterial:"Plastic"
+    },
+    {
+        productId: 5,
+        productTitle:"Custom Key Chain",
+        productCost: 499,
+        productImage:[
+            "https://m.media-amazon.com/images/I/71ZqQVLSjdL._AC_UF350,350_QL80_.jpg",
+            "https://zocivoci.com/cdn/shop/files/91qpvX6OkpL._SL1500.jpg?v=1764847441",
+            "https://rukminim2.flixcart.com/image/480/640/xif0q/table-lamp/b/d/1/3d-personalized-photo-moon-light-7373105930-whatsapp-your-photo-original-imagkegrgkqzthfu.jpeg?q=90"
+        ],
+        productDimensions:"14cm x 14cm x 20cm",
+        productWeight:"250g",
+        productOutputVoltage:"12v",
+        productInputVoltage:"220v",
+        productColor:"White",
+        productFileFields:[
+            {
+                fieldName:"userImage",
+                fieldTitle:"Custom Image",
+            }
+        ],
+        productTextFields:[],
+        productExpectedDeliveryDate:5,
+        productMaterial:"Plastic"
+    },
+    {
+        productId: 6,
+        productTitle:"Custom Trio Box",
+        productCost: 888,
+        productImage:[
+            "https://m.media-amazon.com/images/I/71ZqQVLSjdL._AC_UF350,350_QL80_.jpg",
+            "https://zocivoci.com/cdn/shop/files/91qpvX6OkpL._SL1500.jpg?v=1764847441",
+            "https://rukminim2.flixcart.com/image/480/640/xif0q/table-lamp/b/d/1/3d-personalized-photo-moon-light-7373105930-whatsapp-your-photo-original-imagkegrgkqzthfu.jpeg?q=90"
+        ],
+        productDimensions:"14cm x 14cm x 20cm",
+        productWeight:"250g",
+        productOutputVoltage:"12v",
+        productInputVoltage:"220v",
+        productColor:"White",
+        productFileFields:[
+            {
+                fieldName:"userImage",
+                fieldTitle:"Custom Image",
+            }
+        ],
+        productTextFields:[],
+        productExpectedDeliveryDate:5,
+        productMaterial:"Plastic"
+    },
+    {
+        productId: 7,
+        productTitle:"Custom Key Holder",
+        productCost: 499,
+        productImage:[
+            "https://m.media-amazon.com/images/I/71ZqQVLSjdL._AC_UF350,350_QL80_.jpg",
+            "https://zocivoci.com/cdn/shop/files/91qpvX6OkpL._SL1500.jpg?v=1764847441",
+            "https://rukminim2.flixcart.com/image/480/640/xif0q/table-lamp/b/d/1/3d-personalized-photo-moon-light-7373105930-whatsapp-your-photo-original-imagkegrgkqzthfu.jpeg?q=90"
+        ],
+        productDimensions:"14cm x 14cm x 20cm",
+        productWeight:"250g",
+        productOutputVoltage:"12v",
+        productInputVoltage:"220v",
+        productColor:"White",
+        productFileFields:[
+            {
+                fieldName:"userImage",
+                fieldTitle:"Custom Image",
+            }
+        ],
+        productTextFields:[],
+        productExpectedDeliveryDate:5,
+        productMaterial:"Plastic"
+    },
+    {
+        productId: 8,
+        productTitle:"3D Flip Name",
+        productCost: 450,
+        productImage:[
+            "https://m.media-amazon.com/images/I/71ZqQVLSjdL._AC_UF350,350_QL80_.jpg",
+            "https://zocivoci.com/cdn/shop/files/91qpvX6OkpL._SL1500.jpg?v=1764847441",
+            "https://rukminim2.flixcart.com/image/480/640/xif0q/table-lamp/b/d/1/3d-personalized-photo-moon-light-7373105930-whatsapp-your-photo-original-imagkegrgkqzthfu.jpeg?q=90"
+        ],
+        productDimensions:"14cm x 14cm x 20cm",
+        productWeight:"250g",
+        productOutputVoltage:"12v",
+        productInputVoltage:"220v",
+        productColor:"White",
+        productFileFields:[
+            {
+                fieldName:"userImage",
+                fieldTitle:"Custom Image",
+            }
+        ],
+        productTextFields:[],
+        productExpectedDeliveryDate:5,
+        productMaterial:"Plastic"
+    },
+     {
+        productId: 9,
+        productTitle:"Pen Stand",
+        productCost: 777,
+        productImage:[
+            "https://m.media-amazon.com/images/I/71ZqQVLSjdL._AC_UF350,350_QL80_.jpg",
+            "https://zocivoci.com/cdn/shop/files/91qpvX6OkpL._SL1500.jpg?v=1764847441",
+            "https://rukminim2.flixcart.com/image/480/640/xif0q/table-lamp/b/d/1/3d-personalized-photo-moon-light-7373105930-whatsapp-your-photo-original-imagkegrgkqzthfu.jpeg?q=90"
+        ],
+        productDimensions:"14cm x 14cm x 20cm",
+        productWeight:"250g",
+        productOutputVoltage:"12v",
+        productInputVoltage:"220v",
+        productColor:"White",
+        productFileFields:[
+            {
+                fieldName:"userImage",
+                fieldTitle:"Custom Image",
+            }
+        ],
+        productTextFields:[],
+        productExpectedDeliveryDate:5,
+        productMaterial:"Plastic"
+    },
+];
+
+function startRenderCheckoutPage() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get('productId');
+    const productDetails = productArray.find(product => product.productId == productId);
+    if (productDetails) {
+        renderCheckoutPage(productDetails);
+    } else {
+        console.error("Product not found");
+    }
+}
+
+function renderCheckoutPage(product) {
+    document.getElementById('productTitle').innerText = product.productTitle;
+    document.getElementById('productCost').innerText = product.productCost;
+    document.getElementById('productDimensions').innerText = product.productDimensions;
+    document.getElementById('productWeight').innerText = product.productWeight;
+    document.getElementById('productOutputVoltage').innerText = product.productOutputVoltage;
+    document.getElementById('productInputVoltage').innerText = product.productInputVoltage;
+    document.getElementById('productColor').innerText = product.productColor;
+    document.getElementById('productMaterial').innerText = product.productMaterial;
+
+    const fileFieldsContainer = document.getElementById('fileFieldsContainer');
+
+    if (product.productFileFields.length === 0) {
+        fileFieldsContainer.style.display = 'none';
+    }else{
+        fileFieldsContainer.style.display = 'block';
+        product.productFileFields.forEach(field => {
+        const label = document.createElement('span');
+        label.innerText = field.fieldTitle + ": ";
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.name = field.fieldName;
+        input.className = 'my-2';
+        fileFieldsContainer.appendChild(label);
+        fileFieldsContainer.appendChild(input);
+        });
+    }
+
+    const textFieldsContainer = document.getElementById('textFieldsContainer');
+    
+    if(product.productTextFields.length === 0){
+        textFieldsContainer.style.display = 'none';
+    }else{
+        textFieldsContainer.style.display = 'block';
+        product.productTextFields.forEach(field => {
+        const label = document.createElement('span');
+        label.innerText = field.fieldTitle + ": ";
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.name = field.fieldName;
+        input.className = 'my-2';
+        textFieldsContainer.appendChild(label);
+        textFieldsContainer.appendChild(input);
+    });
+    }
+
+    let imageString = '';
+
+    product.productImage.forEach((imageUrl, index) => {
+        let singleImage = `
+            <div class="swiper-slide">
+                <img src="${imageUrl}" alt="">
+            </div>
+        `;
+        imageString += singleImage;
+    });
+
+    document.getElementById('productImageContainer').innerHTML = imageString;
+
+    document.getElementById('expectedDeliveryDate').innerText = product.productExpectedDeliveryDate + " days";
+}
+
+window.onload = startRenderCheckoutPage;
