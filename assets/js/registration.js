f4672734103f7a18245ec72105112ca6fe2f3705
draptordev-anirudh <72315564+draptorhub@users.noreply.github.com>
2026-05-14 18:18:33 +0530

    Production Values

--- assets/js/registration.js
+++ assets/js/registration.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    // const base_url = "https://api.rapkart.in/"
-    const base_url = "http://localhost:8080/"
+    const base_url = "https://api.rapkart.in/"
+    // const base_url = "http://localhost:8080/"
 
     const form = document.getElementById('registrationForm');
     const workshopRadios = document.querySelectorAll('input[name="workshop"]');
